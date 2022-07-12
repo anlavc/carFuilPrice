d502211dd27b2dc76f883707154805fdfd6ad711
Anıl Avcı <anilavci52@gmail.com>
2022-07-12 10:26:45 +0300

    textbo tıklayınca picker açılır boş alana tıklayınca picker gider.

--- carFuilPrice/ViewController.swift
+++ carFuilPrice/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+   
+    
+    @IBOutlet weak var beginCityTextbox: UITextField!
+    @IBOutlet weak var cityPickerView: UIPickerView!
+  
+       
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+      
     }
+    
+    var cityArray = cityName
 
+        public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+            return 1
+        }
 
+        public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+
+            return cityArray.count
+        }
+
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+            self.view.endEditing(true)
+            return cityArray[row]
+        }
+
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+            self.beginCityTextbox.text = self.cityArray[row]
+          //  self.cityPickerView.isHidden = false
+        }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        beginCityTextbox.endEditing(false)
+        if textField == self.beginCityTextbox {
+                    self.cityPickerView.isHidden = false
+            //boş alana tıklayınca pickeri gizler
+            let gestureRecognizer = UITapGestureRecognizer(target: self,action: #selector(hidePicker))
+            view.addGestureRecognizer(gestureRecognizer)
+            
+               
+            }
+        }
+    
+    @objc func hidePicker() {
+        self.cityPickerView.isHidden = true
+    }
 }
-
